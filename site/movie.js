8b256b8084e999d9885e07588ad3b3226ed1b49c
Jon Lake <jlake21@ford.com>
2022-04-05 11:36:33 -0600

    Add Movie Poster

--- site/movie.js
+++ site/movie.js
@@ -12,13 +12,29 @@ fetch(`https://swapi.dev/api/films/${queryString.get("movie")}`)
         const movieDetail = document.createElement("div")
         movieDetail.classList.add("movie")
         movieDetail.innerHTML = `
+            <img class="poster" src="" />
             <a href="movie.html?movie=${queryString.get("movie")}">${response.title}</a>
-            <time>${response.release_date.slice(0,4)}</time>
+            <time>${response.release_date.slice(0, 4)}</time>
             <h2>Opening Crawl</h2>
             <p>${response.opening_crawl}</p>
             <h2>Characters</h2>
             <ul class="characters">
         `
+        if (response.title === "A New Hope") {
+            fetch(`http://www.omdbapi.com/?t=Star+Wars&&apikey=28629b10`)
+                .then(response => response.json())
+                .then(response => {
+                    const poster = document.querySelector(".poster")
+                    poster.src = `${response.Poster}`
+                })
+        } else {
+            fetch(`http://www.omdbapi.com/?t=${response.title}&&apikey=28629b10`)
+                .then(response => response.json())
+                .then(response => {
+                    const poster = document.querySelector(".poster")
+                    poster.src = `${response.Poster}`
+                })
+        }
         main.append(movieDetail)
         const characters = response.characters.map(character => {
             return fetch(character).then(response => response.json())
